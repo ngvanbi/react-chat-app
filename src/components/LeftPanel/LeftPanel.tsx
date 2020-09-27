e60b2c2523a66f51e4e078686ef53c2054b8389f
Bi Nguyen <bivn@DESKTOP-2GDEHQN.localdomain>
2020-09-27 17:41:57 +0700

    Update left panel

--- src/components/LeftPanel/LeftPanel.tsx
+++ src/components/LeftPanel/LeftPanel.tsx
@@ -1,12 +1,25 @@
-import { createStyles, makeStyles, Theme } from '@material-ui/core';
-import LeftPanelHeader from 'components/LeftPanelHeader/LeftPanelHeader';
+import ChatList from 'components/ChatList/ChatList';
+import Search from 'components/Search/Search';
 import React from 'react';
+
+import { Avatar, CssBaseline, Grid, IconButton, Typography } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { OpenInNew, VideoCall } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       backgroundColor: '#f6f6f6',
       height: '100vh',
+      padding: '5px',
+    },
+    messenger: {
+      marginLeft: '10px',
+    },
+    search: {
+      marginTop: '15px',
+      marginLeft: '5px',
+      marginRight: '5px',
     },
   })
 );
@@ -15,7 +28,29 @@ export default function LeftPanel() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <LeftPanelHeader />
+      <Grid container spacing={0}>
+        <Grid container item spacing={0} xs={6} justify="flex-start" alignItems="center" direction="row">
+          <Avatar>B</Avatar>
+          <Typography className={classes.messenger} variant="subtitle1">
+            <strong>Messenger</strong>
+          </Typography>
+        </Grid>
+        <Grid container item spacing={0} xs={6} justify="flex-end" alignItems="center">
+          <IconButton>
+            <VideoCall />
+          </IconButton>
+          <IconButton>
+            <OpenInNew />
+          </IconButton>
+        </Grid>
+        <Grid item xs={12}>
+          <Search className={classes.search} />
+        </Grid>
+        <CssBaseline />
+        <Grid item xs={12}>
+          <ChatList />
+        </Grid>
+      </Grid>
     </div>
   );
 }
